c04bf71a63bda498e0382756bd4bef5c91bf81c6
whatnameshouldido <pobyjun@naver.com>
2024-07-09 14:20:41 +0900

    Initial commit

---
+++ src/main/java/com/studymavernspringboot/carjpa/VehicleStatus.java
@@ -0,0 +1,4 @@
+package com.studymavernspringboot.carjpa;
+
+public enum VehicleStatus {
+}
---
+++ src/test/java/com/studymavernspringboot/carjpa/CarJpaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.studymavernspringboot.carjpa;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CarJpaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
